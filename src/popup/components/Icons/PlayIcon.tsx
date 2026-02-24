cf8f5dfc369bb32ae929242a6880d2ebc7edc4c0
dxxbletriiiple <dxxbletriiiple@gmail.com>
2026-02-24 19:17:31 +0300

    add: Icons folder with 14 SVG components and index.ts barrel export

---
+++ src/popup/components/Icons/PlayIcon.tsx
@@ -0,0 +1,7 @@
+export function PlayIcon() {
+	return (
+		<svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+			<path d="M8 5v14l11-7z" />
+		</svg>
+	);
+}
---
+++ src/popup/components/Icons/SkipIcon.tsx
@@ -0,0 +1,7 @@
+export function SkipIcon() {
+	return (
+		<svg width="13" height="13" viewBox="0 0 24 24" fill="currentColor">
+			<path d="M6 18l8.5-6L6 6v12zm2.5-6L12 9.5v5L8.5 12zm7.5 6h2V6h-2v12z" />
+		</svg>
+	);
+}
